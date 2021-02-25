5c97c01dc87cd22ad65b0f83ff81e75d347daa34
Noel Crakin <njhanna17@gmail.com>
2021-02-24 23:10:58 -0600

    Added/updated Register page.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
-import { GuardianDashboardPage, LandingPage, SearchPage } from './pages'
+import { GuardianDashboardPage, LandingPage, RegisterPage, SearchPage } from './pages'
 
 function App() {
   return (
     <Switch>
-      <Route path='/' exact={true} component={LandingPage} />
-      <Route path='/search' exact={true} component={SearchPage} />
-      <Route path='/dashboard/:bungieAcct' exact={true} component={GuardianDashboardPage} />
+      <Route path='/' exact={true} component={ LandingPage } />
+      <Route path='/search' exact={true} component={ SearchPage } />
+      <Route path='/dashboard/:bungieAcct' exact={true} component={ GuardianDashboardPage } />
+      <Route path='/register' exact={true} component={ RegisterPage } />
     </Switch>
   );
 }
--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getUserDetails } from '../../utils/api'
+import { Button, Input, InputGroup, FormControl, Icon, Stack, Select, InputLeftAddon } from '@chakra-ui/react'
 
 export function RegisterPage(props)
 {
@@ -9,21 +10,51 @@ export function RegisterPage(props)
     const { history } = props
 
     // Ensures user has authenticated with Discord to protect route from unauthorized access
-    React.useEffect( () => {
-        getUserDetails()
-            .then(({ data }) => {
-                console.log(data)
-                setUser(data)
-                setLoading(false)
-            })
-            .catch((err) => {
-                history.push('/')
-                setLoading(false)
-            })
-    }, [])
-    return !loading && (
+    // React.useEffect( () => {
+    //     getUserDetails()
+    //         .then(({ data }) => {
+    //             console.log(data)
+    //             setUser(data)
+    //             setLoading(false)
+    //         })
+    //         .catch((err) => {
+    //             history.push('/')
+    //             setLoading(false)
+    //         })
+    // }, [])
+    //return !loading && (
+    return (
         <div>
             <h1>Register Page</h1>
+            <form action='submit'>
+                <Stack spacing={3}>
+                    <FormControl isRequired>
+                        <InputGroup>
+                            <InputLeftAddon children={<Icon name='info' />}/>
+                            <Input type='name' placeholder='Account Name' aria-label='Account Name'/>
+                        </InputGroup>
+                    </FormControl>
+                    <FormControl isRequired>
+                        <InputGroup>
+                            <InputLeftAddon children={<Icon name='SettingsIcon' />}/>
+                            <Select placeholder="Select platform" aria-label='Platform'>
+                                <option value="option1">Xbox</option>
+                                <option value="option2">PSN</option>
+                                <option value="option3">Steam</option>
+                                <option value="option3">Stadia</option>
+                            </Select>
+                        </InputGroup>
+                    </FormControl>
+                    <Button
+                        type='submit'
+                        variant='solid'
+                        colorScheme='blue'
+                        _hover={{ boxShadow: 'md' }}
+                        _active={{ boxShadow: 'lg' }}>
+                        Submit
+                    </Button>
+                </Stack>
+            </form>
         </div>
     )
 }
